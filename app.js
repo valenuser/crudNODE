3419a0a4810395eed8d338eb245616b13326ad55
valentinPavon <vpavonlopez02@gmail.com>
2023-03-20 18:44:03 +0100

    seccion de documentacion creada

--- app.js
+++ app.js
@@ -27,9 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
-pool.query('show databases',(err,result)=>{
-  console.log(result);
-})
+
 
 
 // catch 404 and forward to error handler
