e354fa844664d29d3ae01190d7b4c90478f8fc95
valentinPavon <vpavonlopez02@gmail.com>
2023-03-20 16:49:39 +0100

    Server creado

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -38,4 +41,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+module.exports = {app:app,server:server};
