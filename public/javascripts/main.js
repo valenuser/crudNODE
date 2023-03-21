a16e5731bad97c802b030d9b0355a874c2578428
valentinPavon <vpavonlopez02@gmail.com>
2023-03-21 09:44:18 +0100

    diseño para añadir bases de datos finalizado

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,6 +3,9 @@ const sectionDB =  document.getElementById('sectionDB')
 const db = document.querySelector('#db')
 const start = document.querySelector('#start')
 const showDB = document.querySelector('#showDB')
+const addTableDB = document.getElementById('addTableDB')
+const addButtonDB = document.querySelector('#addButtonDB')
+const closeAdd = document.querySelector('#closeAdd')
 
 db.addEventListener('click',()=>{
     main.style.display = 'none'
@@ -12,4 +15,15 @@ db.addEventListener('click',()=>{
 start.addEventListener('click',()=>{
     main.style.display = 'flex'
     sectionDB.style.display = 'none'  
+})
+
+
+addButtonDB.addEventListener('click',()=>{
+    showDB.style.display = 'none'
+    addTableDB.style.display = 'flex'
+})
+
+closeAdd.addEventListener('click',()=>{
+    showDB.style.display = 'flex'
+    addTableDB.style.display = 'none'
 })
